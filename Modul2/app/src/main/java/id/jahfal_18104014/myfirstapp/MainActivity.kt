49d8c4edb206ed29ab63ae5a3cd0a8e8a2cf0dfd
jahfalrizq18104014 <72793484+jahfalrizq18104014@users.noreply.github.com>
2022-02-02 11:50:01 +0700

    18104014_PraktikumPPB2021
    Co-Authored-By: Bagus Bayu Sasongko <bagusbs226@gmail.com>

---
+++ Modul2/app/src/main/java/id/jahfal_18104014/myfirstapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.jahfal_18104014.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
